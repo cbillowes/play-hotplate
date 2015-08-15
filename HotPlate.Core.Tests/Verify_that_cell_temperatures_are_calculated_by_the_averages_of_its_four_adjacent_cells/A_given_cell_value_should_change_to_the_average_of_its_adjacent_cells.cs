16f6163569649d6da4973434339b0c3b92c056a1
Clarice Bouwer <cbillowes@gmail.com>
2015-08-15 10:17:21 +0200

    Added tests and implementation to verify that cell temperatures are calculated by the averages of its four adjacent cells.

---
+++ HotPlate.Core.Tests/Verify_that_cell_temperatures_are_calculated_by_the_averages_of_its_four_adjacent_cells/A_given_cell_value_should_change_to_the_average_of_its_adjacent_cells.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+// ReSharper disable once CheckNamespace
+namespace HotPlate.Core.Tests
+{
+    [TestFixture]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    class A_given_cell_value_should_change_to_the_average_of_its_four_adjacent_cells
+    {
+        private CellTemperatureCalculator cellTemperatureCalculator;
+        private int row;
+        private int column;
+        private float actual;
+        private float expected;
+
+        [SetUp]
+        public void Setup()
+        {
+            var cells = new float[,]
+            {
+                {10f, 20f, 30f},
+                {40f, 50f, 60f},
+                {70f, 80f, 90f}
+            };
+            cellTemperatureCalculator = new CellTemperatureCalculator(cells);
+        }
+
+        [Test]
+        public void Verify_first_cell_in_first_row()
+        {
+            row = 1;
+            column = 1;
+            expected = 15;
+            Act_and_assert();
+        }
+
+        [Test]
+        public void Verify_second_cell_in_first_row()
+        {
+            row = 1;
+            column = 2;
+            expected = 22.5f;
+            Act_and_assert();
+        }
+
+        [Test]
+        public void Verify_third_cell_in_first_row()
+        {
+            row = 1;
+            column = 3;
+            expected = 20;
+            Act_and_assert();
+        }
+
+        [Test]
+        public void Verify_first_cell_in_second_row()
+        {
+            row = 2;
+            column = 1;
+            expected = 32.5f;
+            Act_and_assert();
+        }
+
+        [Test]
+        public void Verify_third_cell_in_last_row()
+        {
+            row = 3;
+            column = 3;
+            expected = 35f;
+            Act_and_assert();
+        }
+
+        private void Act_and_assert()
+        {
+            actual = cellTemperatureCalculator.GetAverage(row - 1, column - 1);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ HotPlate.Core/CellTemperatureCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace HotPlate.Core
+{
+    public class CellTemperatureCalculator
+    {
+        private readonly float[,] cells;
+
+        public CellTemperatureCalculator(float[,] cells)
+        {
+            this.cells = cells;
+        }
+        
+        public float GetAverage(int row, int column)
+        {
+            var top = (row - 1 >= 0) ? cells[row - 1, column] : 0f;
+            var right = (column + 1 <= cells.GetUpperBound(1)) ? cells[row, column + 1] : 0f;
+            var bottom = (row + 1 <= cells.GetUpperBound(0)) ? cells[row + 1, column] : 0f;
+            var left = (column - 1 >= 0) ? cells[row, column - 1] : 0f;
+
+            return (top + right + bottom + left)/4;
+        }
+    }
+}
