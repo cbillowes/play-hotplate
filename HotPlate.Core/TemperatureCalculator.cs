e255805b8300b1dd4856a46e3674f41b00f7f78c
Clarice Bouwer <cbillowes@gmail.com>
2015-08-15 15:44:36 +0200

    Refactored temperature calculator to be more readable.

--- HotPlate.Core/TemperatureCalculator.cs
+++ HotPlate.Core/TemperatureCalculator.cs
@@ -5,30 +5,53 @@ namespace HotPlate.Core
 {
     public class TemperatureCalculator
     {
+        private List<float> averages;
+        
         public float GetAverage(float[,] cells, int row, int column)
         {
-            var averages = new List<float>();
-            if (HasTop(row)) averages.Add(GetTop(cells, row, column));
-            if (HasRight(cells, column)) averages.Add(GetRight(cells, row, column));
-            if (HasBottom(cells, row)) averages.Add(GetBottom(cells, row, column));
-            if (HasLeft(column)) averages.Add(GetLeft(cells, row, column));
+            averages = new List<float>();
+            AddTemperatures(cells, row, column);
             return averages.Average();
         }
 
-        private bool HasTop(int row) => row - 1 >= 0;
+        private void AddTemperatures(float[,] cells, int row, int column)
+        {
+            AddTopTemperature(cells, row, column);
+            AddRightTemperature(cells, row, column);
+            AddBottomTemperature(cells, row, column);
+            AddLeftTemperature(cells, row, column);
+        }
 
-        private bool HasRight(float[,] cells, int column) => column + 1 <= cells.GetUpperBound(1);
+        private void AddTopTemperature(float[,] cells, int row, int column)
+        {
+            if (row - 1 >= 0)
+            {
+                averages.Add(cells[row - 1, column]);
+            }
+        }
 
-        private bool HasBottom(float[,] cells, int row) => row + 1 <= cells.GetUpperBound(0);
+        private void AddRightTemperature(float[,] cells, int row, int column)
+        {
+            if (column + 1 <= cells.GetUpperBound(1))
+            {
+                averages.Add(cells[row, column + 1]);
+            }
+        }
 
-        private bool HasLeft(int column) => column - 1 >= 0;
+        private void AddBottomTemperature(float[,] cells, int row, int column)
+        {
+            if (row + 1 <= cells.GetUpperBound(0))
+            {
+                averages.Add(cells[row + 1, column]);
+            }
+        }
 
-        private float GetTop(float[,] cells, int row, int column) => cells[row - 1, column];
-
-        private float GetRight(float[,] cells, int row, int column) => cells[row, column + 1];
-
-        private float GetBottom(float[,] cells, int row, int column) => cells[row + 1, column];
-
-        private float GetLeft(float[,] cells, int row, int column) => cells[row, column - 1];
+        private void AddLeftTemperature(float[,] cells, int row, int column)
+        {
+            if (column - 1 >= 0)
+            {
+                averages.Add(cells[row, column - 1]);
+            }
+        }
     }
 }
